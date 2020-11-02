3ccbe16a6a330780f67a5fa27223e2c490620ae8
Mostafa Saad <mostafa_ahmed20155@yahoo.com>
2020-11-02 15:47:14 +0200

    EN and AR Added with Product Qty Counter

--- js/main.js
+++ js/main.js
@@ -6,3 +6,35 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+$(".button").on("click", function() {
+
+  var $button = $(this);
+  var oldValue = $button.parent().find("input").val();
+
+  if ($button.text() == "+") {
+	  var newVal = parseFloat(oldValue) + 1;
+	} else {
+   // Don't allow decrementing below zero
+    if (oldValue > 0) {
+      var newVal = parseFloat(oldValue) - 1;
+    } else {
+      newVal = 0;
+    }
+  }
+
+  $button.parent().find("input").val(newVal);
+
+});
+
+// submitForms = function(){
+//   document.getElementById("instructions-form").submit();
+//   document.getElementById("qty-form").submit();
+// }
+
+submitForms = function(){
+  document.getElementById("instructions-form").submit();
+  
+  setTimeOut(function() {
+    document.getElementById("qty-form").submit()
+  }, 5000);
+}

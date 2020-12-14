8857c57415f70c28cb0080850c2d8b2758b10a85
Mostafa Saad <mostafa_ahmed20155@yahoo.com>
2020-12-14 16:59:56 +0200

    add-to-cart-toss

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function openNav() {
   $('#mySidenav > *').hide(0);
-  $('#mySidenav').removeClass('in-active'); 
+  $('#mySidenav').removeClass('in-active');
   document.getElementById("mySidenav").style.width = "34.5rem";
   // $('#mySidenav').animate({width: '34.5rem'}, 500);
-  setTimeout(function(){
+  setTimeout(function () {
     // console.log("show");
     $('#mySidenav > *').show(0);
   }, 500)
@@ -12,20 +12,20 @@ function openNav() {
 
 function closeNav() {
   // document.getElementById("mySidenav").style.width = "0";
-  $('#mySidenav > *').hide(0, function(){
+  $('#mySidenav > *').hide(0, function () {
     document.getElementById("mySidenav").style.width = "0";
-});
+  });
 }
 
-$(".button").on("click", function() {
+$(".button").on("click", function () {
 
   var $button = $(this);
   var oldValue = $button.parent().find("input").val();
 
   if ($button.text() == "+") {
-	  var newVal = parseFloat(oldValue) + 1;
-	} else {
-   // Don't allow decrementing below zero
+    var newVal = parseFloat(oldValue) + 1;
+  } else {
+    // Don't allow decrementing below zero
     if (oldValue > 0) {
       var newVal = parseFloat(oldValue) - 1;
     } else {
@@ -42,32 +42,40 @@ $(".button").on("click", function() {
 //   document.getElementById("qty-form").submit();
 // }
 
-submitForms = function(){
+submitForms = function () {
   document.getElementById("instructions-form").submit();
-  
-  setTimeOut(function() {
+
+  setTimeOut(function () {
     document.getElementById("qty-form").submit()
   }, 5000);
 }
 
 
-$(document).ready(function(){
-  $(".product-size-options .single-option").click(function(){
-	  $(".product-size-options .single-option").removeClass("selected-custom");
+$(document).ready(function () {
+  $(".product-size-options .single-option").click(function () {
+    $(".product-size-options .single-option").removeClass("selected-custom");
     $(this).addClass("selected-custom");
   });
-  $(".style-custom-options .single-option").click(function(){
-	  $(".style-custom-options .single-option").removeClass("selected-custom");
+  $(".style-custom-options .single-option").click(function () {
+    $(".style-custom-options .single-option").removeClass("selected-custom");
     $(this).addClass("selected-custom");
   });
-  $(".color-options .single-option").click(function(){
-	  $(".color-options .single-option").removeClass("selected");
+  $(".color-options .single-option").click(function () {
+    $(".color-options .single-option").removeClass("selected");
     $(this).addClass("selected");
   });
-  $(".custom-product-option").click(function(){
+  $(".custom-product-option").click(function () {
     $(this).toggleClass("selected-custom");
   });
-  $(".custom-product-label").click(function(){
-    $(this).parent(".custom-product-container").toggleClass("selected-list",1000);
-    });
+  $(".custom-product-label").click(function () {
+    $(this).parent(".custom-product-container").toggleClass("selected-list", 1000);
+  });
+  $(".single-option").click(function () {
+    $(this).find("input").prop("checked", true);
+    // $("input[name=size]").val($(this).find("input").attr("value"))
+    // $("input[name=size]").removeAttr("checked")
+    // $(this).find("input").attr("checked","checked");
+    // console.log($(this).find("input").attr("value"))
+  })
 });
+
